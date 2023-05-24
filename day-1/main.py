f56c2916cc5043418f065710db6f58c112ed8d81
Lukas Hauser <hauserlukas1998@gmail.com>
2023-05-24 08:27:14 +0200

    finished day1 project

---
+++ day-1/main.py
@@ -0,0 +1,10 @@
+#create greeting
+print("Hello")
+#get city name
+city = input("Which city did u grew up in? \n")
+#get pet name
+pet = input("What's the name of your pet? \n")
+#combine to band name
+bandname = city + " " + pet
+
+print(f"your band name will be {city} {pet} or {bandname}")
